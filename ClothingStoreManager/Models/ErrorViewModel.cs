4091e6b14e799fafa5e14280c29e7babcebd740e
hung211200941@lms.utc.edu.vn <hung211200941@lms.utc.edu.vn>
2023-09-29 22:09:39 +0700

    Add project files.

---
+++ ClothingStoreManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClothingStoreManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
